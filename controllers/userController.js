f5ec4a3aa485c0e439b50a0942899e6730062095
kchu01 <chukathy01@gmail.com>
2021-02-25 14:45:53 -0800

    Add Cookies

---
+++ controllers/userController.js
@@ -0,0 +1,57 @@
+const router = require('express').Router()
+const db = require('../models')
+
+/** NEW USERS **/
+// GET ROUTE - /users/new
+router.get('/new', (req, res) => {
+    res.render("users/new")
+})
+
+// POST ROUTE - /users
+router.post('/', async (req, res) => {
+    try {
+        const user = await db.user.create({
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.cookie('userId', user.id)
+
+        res.redirect('/')
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+/** LOGIN **/
+// GET ROUTE to login page - /users/login
+router.get('/login', (req, res) => {
+    res.render("users/login")
+})
+
+// POST ROUTE - USER LOGIN PASSWORD 
+router.post('/login', async (req, res) => {
+    // res.send('you submitted a login form fam')
+
+    // look up user who has the incoming email and check if it matches the incoming password
+    const user = await db.user.findOne({
+        where: { email: req.body.email }
+    })
+    // the cookie keeps them logged in
+    // if yes, set the cookie userId = user.id
+    if (user.password === req.body.password) {
+        res.cookie('userId', user.id)
+        res.redirect('/')
+    } else {
+        // if no, render the login form
+        res.render('users/login')
+    }
+})
+
+/** LOGOUT */
+router.get('/logout', (req, res) => {
+    res.clearCookie('userId')
+    res.redirect('/')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const { render } = require('ejs')
 const express = require('express')
 const app = express()
 const rowdy = require('rowdy-logger')
+const cookieParser = require('cookie-parser')
 
 // middleware
 const rowdyRes = rowdy.begin(app)
@@ -8,12 +10,18 @@ app.use(require('morgan')('tiny'))
 app.set('view engine', 'ejs')
 app.use(require('express-ejs-layouts'))
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public')) // tells it to grab from public folder
+app.use(cookieParser())
 
 // routes
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+// controllers
+app.use('/users', require('./controllers/userController.js'))
+
+// starts the sever!
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log('server started!');
